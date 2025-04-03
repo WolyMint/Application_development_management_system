e87bff93356e8e1a403cbe1a49edad67e540c4aa
user <@WolyMint>
2025-04-03 21:56:48 +0400

    made user

---
+++ src/main/java/com/example/task/model/User.java
@@ -0,0 +1,10 @@
+package com.example.task.model;
+
+public class User {
+
+    private Long id;
+    private String name;
+    private String email;
+    private String birth;
+    private int age;
+}
